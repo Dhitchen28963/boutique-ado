26ca958942992eb4296dd00772c15420fcbb0a25
David Hitchen <hitchen28963@yahoo.com>
2024-09-18 12:01:27 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'sk_test_51Q0MMPRxTofVsr1TTwb26v30fBrjKxfqEcuMMkEoVtyDaEKu5nHEqQGXjEYpUieb4ay6ZzFnnKwPhY8yW1vrYLTH00pZp1pLdR',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
