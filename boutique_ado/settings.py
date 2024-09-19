28c39002a58c7aa4b459da68256b302cc6f062f3
David Hitchen <hitchen28963@yahoo.com>
2024-09-19 19:48:56 +0000

    Add Procfile and configure static files

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ if os.path.isfile('env.py'):
 SECRET_KEY = 'django-insecure-9$8^4(z^u(q)!ig(blgljbkh@m!kiernuz%l@(a0vdtt7q1es^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-dhitchen289-boutiqueado-339n7vz1xx5.ws.codeinstitute-ide.net']
 
@@ -167,6 +167,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
